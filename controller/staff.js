e18ae8014fdbb354505ec32353a3e26baac0c0de
sureshkumar2001 <iamsureshkumar2001@users.noreply.github.com>
2024-05-21 15:30:43 +0530

    update

--- controller/staff.js
+++ controller/staff.js
@@ -3,7 +3,6 @@ const Purchase = require("../models/purchase");
 const Sales = require("../models/sales");
 const excel = require("exceljs");
 
-
 // Add Post
 const addStaff = (req, res) => {
   console.log("req: ", req.body.userId);
@@ -24,8 +23,6 @@ const addStaff = (req, res) => {
     foodadv: req.body.foodadv,
     balanceepay: req.body.balanceepay,
     remark: req.body.remark,
-   
-
   });
 
   addStaff
@@ -66,21 +63,21 @@ const updateSelectedStaff = async (req, res) => {
       { _id: req.body.staffID },
       {
         userID: req.body.userId,
-    sname: req.body.sname,
-    nodwork: req.body.nodwork,
-    festivelh: req.body.festivelh,
-    totaldays: req.body.totaldays,
-    otdays: req.body.otdays,
-    wages: req.body.wages,
-    salarytobe: req.body.salarytobe,
-    perhour: req.body.perhour,
-    ot: req.body.ot,
-    month: req.body.month,
+        sname: req.body.sname,
+        nodwork: req.body.nodwork,
+        festivelh: req.body.festivelh,
+        totaldays: req.body.totaldays,
+        otdays: req.body.otdays,
+        wages: req.body.wages,
+        salarytobe: req.body.salarytobe,
+        perhour: req.body.perhour,
+        ot: req.body.ot,
+        month: req.body.month,
 
-    totalsalary: req.body.totalsalary,
-    foodadv: req.body.foodadv,
-    balanceepay: req.body.balanceepay,
-    remark: req.body.remark,
+        totalsalary: req.body.totalsalary,
+        foodadv: req.body.foodadv,
+        balanceepay: req.body.balanceepay,
+        remark: req.body.remark,
       },
       { new: true }
     );
@@ -104,42 +101,53 @@ const searchStaff = async (req, res) => {
 // Get All Staff
 const getExport = async (req, res) => {
   try {
-    const users = await Material.find().sort({ createdAt: 1 });
+    const users = await Staff.find().sort({ createdAt: 1 });
 
     const workbook = new excel.Workbook();
     const worksheet = workbook.addWorksheet("Users");
 
     worksheet.columns = [
-      { header: "Supplier Name", key: "name", width: 20 },
-      { header: "Date", key: "date", width: 20 },
-      { header: "Material Name", key: "mname", width: 20 },
-      { header: "Units", key: "units", width: 20 },
-      { header: "Quantity", key: "quantity", width: 20 },
-      { header: "Size", key: "size", width: 20 },
-      { header: "Thickness", key: "thickness", width: 20 },
-      { header: "Length", key: "length", width: 20 },
-      { header: "Current Stock", key: "cstock", width: 20 },
-      { header: "Stock", key: "stock", width: 20},
+      { header: "Name", key: "sname", width: 20 },
+      { header: " No Of Day Working", key: "nodwork", width: 20 },
+      { header: "Fesivel Holiday", key: "festivelh", width: 20 },
+      { header: " Total Days", key: "totaldays", width: 20 },
+      { header: "OT Hours", key: "otdays", width: 20 },
+      { header: "PER Day Wages ", key: "size", width: 20 },
+      { header: "Salary To Be", key: "salarytobe", width: 20 },
+      { header: "PER Hour", key: "perhour", width: 20 },
+      { header: " OT", key: "ot", width: 20 },
+      { header: " Month", key: "month", width: 20 },
+      { header: "   Total Salary ", key: "totalsalary", width: 20 },
+      { header: "   Food Advance ", key: "foodadv", width: 20 },
+      { header: "   Balance E-Pay ", key: "balanceepay", width: 20 },
+      { header: "   Remark ", key: "remark", width: 20 },
+
       { header: "Created At", key: "createdAt", width: 20 },
     ];
 
     users.forEach((user) => {
       worksheet.addRow({
-        name: user.name,
-        date: user.date,
-        mname: user.mname,
-        units: user.units,
-        quantity: user.quantity,
-        size: user.size,
-        thickness: user.thickness,
-        length: user.length,
-        cstock: user.cstock,
-        stock: user.stock,
+        sname: user.sname,
+        nodwork: user.nodwork,
+        festivelh: user.festivelh,
+        totaldays: user.totaldays,
+        otdays: user.otdays,
+        wages: user.wages,
+        salarytobe: user.salarytobe,
+        perhour: user.perhour,
+        ot: user.ot,
+        month: user.month,
+        totalsalary: user.totalsalary,
+
+        foodadv: user.foodadv,
+        balanceepay: user.balanceepay,
+        remark: user.remark,
+
         createdAt: user.createdAt.toLocaleDateString(),
       });
     });
     const timestamp = new Date().toISOString().replace(/:/g, "-"); // Generate timestamp for filename
-    const fileName = `material_${timestamp}.xlsx`; // Construct filename with timestamp
+    const fileName = `staff_${timestamp}.xlsx`; // Construct filename with timestamp
     res.setHeader(
       "Content-Type",
       "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
